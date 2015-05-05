055aa8203bbd7176ac03747c99864f2660f113af
Liu Jin <coolziljin@gmail.com>
2015-05-05 16:32:51 +0800

    add summary in project caption view

--- DrawCrowd/Project.m
+++ DrawCrowd/Project.m
@@ -11,14 +11,21 @@
 @implementation Project
 
 - (NSAttributedString *)attributedCaptionTitle {
-    NSDictionary *attributes = @{NSFontAttributeName: [UIFont systemFontOfSize:13.0],
-                                 NSForegroundColorAttributeName: [UIColor whiteColor]};
+    NSDictionary *attrs = @{NSFontAttributeName: [UIFont systemFontOfSize:13.0], NSForegroundColorAttributeName: [UIColor whiteColor]};
     if (!_title) {
-        return [[NSAttributedString alloc] initWithString:@"" attributes:attributes];
+        return [[NSAttributedString alloc] initWithString:@"" attributes:attrs];
     } else {
-        return [[NSAttributedString alloc] initWithString:_title attributes:attributes];
+        return [[NSAttributedString alloc] initWithString:_title attributes:attrs];
     }
+}
 
+- (NSAttributedString *)attributedCaptionSummary {
+    NSDictionary *attrs = @{NSFontAttributeName: [UIFont systemFontOfSize:10.0], NSForegroundColorAttributeName: [UIColor colorWithWhite:0.8 alpha:1.000]};
+    if (!_desc) {
+        return [[NSAttributedString alloc] initWithString:@"" attributes:attrs];
+    } else {
+        return [[NSAttributedString alloc] initWithString:_desc attributes:attrs];
+    }
 }
 
 @end
